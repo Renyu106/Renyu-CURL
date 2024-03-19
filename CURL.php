3ae40a123a1a8fb76dad47f60729f5bfc1a1e4b2
Renyu106 <admin@winsub.kr>
2024-03-19 16:54:27 +0900

    Update CURL.php

--- CURL.php
+++ CURL.php
@@ -1,5 +1,4 @@
 <?php
-namespace LIB;
 
 class CURL{
 
@@ -10,7 +9,7 @@ class CURL{
     private $CURL_DEFAULT_USER_AGENT = "Renyu-CURL/1.0";
     private $CURL_REDIRECT_ALLOW = false;
     private $OPTIMIZE_CLOUDFLARE = true;
-    private $CLOUDFLARE_ENTERPRISE_DOMAIN = "namu.wiki";
+    private $CLOUDFLARE_ENTERPRISE_DOMAIN = "arca.live";
     
     public function __construct($URL, $METHOD="GET"){
         $this->CURL_URL = $URL;
@@ -78,18 +77,18 @@ class CURL{
 
         try{
             $EXEC = curl_exec($CURL);
-            $HEADER_SIZE = curl_getinfo($CURL, CURLINFO_HEADER_SIZE);
-            $HTTP_CODE = curl_getinfo($CURL, CURLINFO_HTTP_CODE);
+            $CURL_INFO = curl_getinfo($CURL);
             curl_close($CURL);
-            if($HTTP_CODE == 0) return self::RETURN("ERR", curl_error($CURL), curl_errno($CURL));
-            $FORMATED_HEADERS = array_reduce(explode("\n", substr($EXEC, 0, $HEADER_SIZE)), function($carry, $header) {
+            if($CURL_INFO['http_code'] == 0) return self::RETURN("ERR", curl_error($CURL), curl_errno($CURL));
+            $FORMATED_HEADERS = array_reduce(explode("\n", substr($EXEC, 0, $CURL_INFO['header_size'])), function($carry, $header) {
                 list($key, $value) = array_pad(explode(': ', $header, 2), 2, null);
                 if($key && $value) $carry[trim($key)] = trim($value);
                 return $carry;
             }, []);
-            return self::RETURN("OK", "FETCHED", $HTTP_CODE, array(
-                "RESPONSE" => array("DATA" => substr($EXEC, $HEADER_SIZE), "HEADER" => $FORMATED_HEADERS),
-                "REQUEST" => array("DATA" => $this->CURL_PAYLOAD, "HEADER" => $this->CURL_HEADERS),
+
+            return self::RETURN("OK", "FETCHED", $CURL_INFO['http_code'], array(
+                "RESPONSE" => array("DATA" => substr($EXEC, $CURL_INFO['header_size']), "HEADER" => $FORMATED_HEADERS, "META" => array_change_key_case($CURL_INFO, CASE_UPPER)),
+                "REQUEST" => array("DATA" => $this->CURL_PAYLOAD, "HEADER" => $this->CURL_HEADERS)
             ));
         } catch (\Exception $e) {
             return self::RETURN("ERR", $e->getMessage());
